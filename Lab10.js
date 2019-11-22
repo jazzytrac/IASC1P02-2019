86cd14d8c7a33f04e2bbfa2ee0a84e10a9372e09
Jasmine <jazzytrac@ymail.com>
2019-11-21 21:23:51 -0500

    Lab10

--- Lab10.js
+++ Lab10.js
@@ -4,6 +4,27 @@ var two= document.getElementById("Numbertwo");
 function addition() {
 
   var calculation = Number(one.value) + Number(two.value);
+  document.getElementById("Answer").innerHTML= calculation;
+
+}
+
+function subtraction() {
+
+  var calculation = Number(one.value) + Number(two.value);
+  document.getElementById("Answer").innerHTML=calculation;
+
+}
+
+function multiplication() {
+
+  var calculation = Number(one.value) + Number(two.value);
+  document.getElementById("Answer").innerHTML=calculation;
+
+}
+
+function division() {
+
+  var calculation = Number(one.value) + Number(two.value);
   document.getElementById("Answer").innerHTML=calculation;
 
 }
